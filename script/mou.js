f2153109e19b994e798f5885fe28702cb990c955
donghyenshin <80297371+donghyenshin@users.noreply.github.com>
2021-09-01 15:50:44 +0900

    Add files via upload

---
+++ script/mou.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".move").click(function() {
+        $(".popup").show();
+    });
+    $(".line-box").click(function() {
+        $(".popup").hide();
+
+    });
+
+});
+
